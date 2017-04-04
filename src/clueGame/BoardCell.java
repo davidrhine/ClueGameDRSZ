b3e5923599df914865235de12b0c95bcef1cbc63
Sean Zusi <szusi@mymail.mines.edu>
2017-04-04 16:10:08 -0600

    cleaned up whitespace and unneeded comments in BoardCell

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -9,6 +9,7 @@ public BoardCell(){
 	row = 0;
 	column = 0;
 }
+
 public BoardCell(int row, int column, char initial, DoorDirection door) {
 	super();
 	this.row = row;
@@ -16,49 +17,61 @@ public BoardCell(int row, int column, char initial, DoorDirection door) {
 	this.initial = initial;
 	this.door = door;
 }
+
 public BoardCell(int row, int column) {
 	super();
 	this.row = row;
 	this.column = column;
 }
+
 public int getRow() {
 	return row;
 }
+
 @Override
 public String toString() {
 	return "BoardCell [row=" + row + ", column=" + column + "]";
 }
+
 public void setRow(int row) {
 	this.row = row;
 }
+
 public int getColumn() {
 	return column;
 }
+
 public void setColumn(int column) {
 	this.column = column;
 }
+
 public boolean isDoorway() {
-	// TODO Auto-generated method stub
+	
 	if(door == DoorDirection.NONE) return false;
 	else return true;
 }
+
 public boolean isWalkway() {
-	// TODO Auto-generated method stub
+	
 	if(initial == 'W') return true;
 	else return false;
 }
+
 public boolean isRoom() {
-	// TODO Auto-generated method stub
+	
 	if(initial == 'W' || initial == 'X' ) return false;
 	else return true;
 }
+
 public DoorDirection getDoorDirection() {
 	return door;
-	// TODO Auto-generated method stub
+	
 	
 }
+
 public char getInitial() {
 	return initial;
 	
-}
+	}
+
 }
